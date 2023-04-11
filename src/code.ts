23230bd5800b65199fdc259fe44e4f85b2ee5971
Saksit Sawang <hanamiza555@gmail.com>
2023-04-12 01:28:40 +0700

    sol

--- src/code.ts
+++ src/code.ts
@@ -1,16 +1,28 @@
 import { testFn } from './lib.ts'
 
-type Input = {
-  // Input type
+export type Input = {
+  [key: string]: number[]
 }
-type Output = {
-  // Output type
+export type Output = {
+  [key: string]: number[] | string
 }
 
-// Implement your function here
+export const findAlmostMinMax = (arr: number[]) => {
+  const set = new Set(arr)
+  if (set.size < 3) {
+    return 'None'
+  }
+  const sorted = [...set].sort((a, b) => a - b)
+  return [sorted[1], sorted[sorted.length - 2]]
+}
 const fn = (input: Input) => {
-  console.log(input)
-  const output: Output = {}
+  const output: Output = Object.entries(input).reduce(
+    (prev, [key, arr]) => ({
+      ...prev,
+      [key]: findAlmostMinMax(arr),
+    }),
+    {},
+  )
   return output
 }
 
